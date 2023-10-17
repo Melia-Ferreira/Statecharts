79cd7cec0356f3aaaf4710c7f0670d543002d312
Melia-Ferreira <melia.ferreira@etud.univ-jfc.fr>
2023-10-17 09:45:47 +0200

    Final commit

--- pages/xstatepolyline/xStatePolyline.js
+++ pages/xstatepolyline/xStatePolyline.js
@@ -16,11 +16,60 @@ let polyline // La polyline en cours de construction;
 
 const polylineMachine = createMachine(
     {
-        /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAcsAbATwBkBLDMfEI2SgF0qwzoA9EBaANnVI9eAOgAM4iZMkB2ZGnokK1MMMoRitJAsYs2nRABYATAMQAOAIzCD0gJwXetgwGZezgw9ty5QA */
+        /** @xstate-layout N4IgpgJg5mDOIC5QAcD2AbAngGQJYDswA6XCdMAYgFkB5AVQGUBRAYWwEkWBpAbQAYAuohSpYuAC65U+YSAAeiAMwA2IgBY+mgIx8NATgCsBvWuUAaEJkQBaAExqifAOx8AHFpcHbfRX2V+AXwCLNCw8QiIAETgxfAACAGoIMDi0AnFYanpmWgA1Jn4hJBA0MUlpWQUENRqiRUUDLS01Iw8PZQMLKwRrZodlWwGBrVcVPWU9IJCMHAJiaNhYuK00-HEsxlYObkLZUokpGWKqxXH1FxUJg1O9Vz5Oy0RXWyIJ5ScTAw7lCdtbKZKM3C8xiBESyVSqHSmQAQgBDADGAGtYMhEWBdsV9uUjqAqjoPnUGuMWipFGpbF1EDo+ERRlpbL4dFoVE5GQDQrMIgslkkUqsMhQmGswAAnTEiMqHSqIbxaOp8PRsslONTPVyuKk9RmKIiGPwmZrKLQDCkcoFzKKg+IrKFrIWwBFw5AYwR7UQHCrHRAGVx02weAymd7XY1qLUMv0BgwfZQNH56Nz-YKAsKWnlgvmQ6EOp0uiWAqVevHUr56ln+DyMxN6QxaxmqO4fDW2PTktXJ6Zp7nW8H8u2C2ibNicXhurEenEyhA6WrKlpBmODX1a3oqIhOVXuPgEmMeNTm7sgxaZiECzJDnI0fI8LRFSWe3HyJ5uOm+Ny+FxqJrhx4IRStnq9hkguSo+E4QQpvgqDJPAE5Hu6RZPlUvS1BomgGq4nytpqf7WAB8q2AYfiuE45J3Puh5csQpDkIhj7TtYZzoZoExYS0OGrvKygtO4xHKO4EwMm4VHAlaJ42gK9FTt6CC2E4frGLWtaKAywm4d0vSvLxJpqHokabuMnaptR4m8meA5wQ+MklggzwODu9y3DhqmnFx2m+rp+m2KRHxKpBARAA */
         id: "polyLine",
+
         initial: "idle",
+
         states : {
             idle: {
+                on: {
+                    MOUSECLICK: {
+                        target: "Dessin 1point",
+                        actions: "createLine"
+                    }
+                }
+            },
+
+            "Dessin 1point": {
+                on: {
+                    Escape: {
+                        target: "idle",
+                        actions: "abandon"
+                    }
+                }
+            },
+
+            "Dessin +de points": {
+                on: {
+                    MOUSEMOVE: [{
+                        target: "Dessin 1point",
+                        actions: "setLastPoint",
+                        cond: "currentsPoints.length = 3"
+                    }, {
+                        target: "Dessin +de points",
+                        internal: true
+                    }],
+
+                    Backspace: {
+                        target: "Dessin +de points",
+                        actions: "removeLastPoint",
+                        internal: true
+                    },
+
+                    Enter: {
+                        target: "idle",
+                        actions: "saveLine"
+                    },
+
+                    Escape: "idle",
+
+                    MOUSECLICK: {
+                        target: "Dessin +de points",
+                        internal: true,
+                        cond: "pasPlein"
+                    }
+                }
             }
         }
     },
